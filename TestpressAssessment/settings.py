7770a06b88e77661506b22de9747950b9a98ff94
Rahul Sharma <bbblaze27@gmail.com>
2021-04-03 20:21:52 +0530

    Hide Secret key and debug option

--- TestpressAssessment/settings.py
+++ TestpressAssessment/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mysq+5u@$5zd6obw&$@+ib^3)1pw$v&n36@c(gai8(wfmtqd4!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['locallibrary-tp-assessment.herokuapp.com', '127.0.0.1']
 
