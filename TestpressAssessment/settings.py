afb8cac38dd335034fc2d89238c6ed03deec61f2
Rahul Sharma <bbblaze27@gmail.com>
2021-04-03 20:26:17 +0530

    Fix allowed hosts

--- TestpressAssessment/settings.py
+++ TestpressAssessment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['locallibrary-tp-assessment.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['testpress-assessment.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
